e1b48a2765e6cd52c35fbcb02b808f47dbb8e229
YukiUmetsu <yuuki.umetsu@gmail.com>
2023-02-18 21:28:48 -0600

    add comments with reference links

--- gcp/cloud-storage/main.tf
+++ gcp/cloud-storage/main.tf
@@ -1,3 +1,4 @@
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/storage_bucket#storage_class
 resource "google_storage_bucket" "bucket_from_tf_02182023" {
     name = "bucket_from_tf_02182023"
     location = "US"
@@ -19,12 +20,14 @@ resource "google_storage_bucket" "bucket_from_tf_02182023" {
     }
 }
 
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/storage_bucket_object
 resource "google_storage_bucket_object" "gcp_tf_image" {
   name   = "gcp_tf_image"
   bucket = google_storage_bucket.bucket_from_tf_02182023.name
   source = "./gcp-terraform.png"
 }
 
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/storage_object_access_control
 resource "google_storage_object_access_control" "public_rule" {
   object = google_storage_bucket_object.gcp_tf_image.name
   bucket = google_storage_bucket.bucket_from_tf_02182023.name
